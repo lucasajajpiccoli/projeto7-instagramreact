20ac669d79539f7159cb44c57b650f2f82eb66e4
lucasajajpiccoli <lucasajaj@gmail.com>
2022-07-18 10:32:41 -0300

    Segundo componente: Navbar

---
+++ src/Componentes/Navbar.js
@@ -0,0 +1,48 @@
+function Logo() {
+    return (
+        <div class="logo">
+            <ion-icon name="logo-instagram"></ion-icon>
+            <div class="separador"></div>
+            <img src="assets/img/logo.png" />
+        </div>
+    );
+}
+
+function MobileIcone(props) {
+    return (
+        <div class={props.class}>
+            <ion-icon name={props.name}></ion-icon>
+        </div>
+    );
+}
+
+function Navbar() {
+    const icons = ["paper-plane", "compass", "heart", "person"];
+
+    return (
+        <div class="navbar">
+            <div class="container">
+                <Logo />
+
+                <MobileIcone class="logo-mobile" name="logo-instagram" />
+
+                <div class="instagram-mobile">
+                    <img src="assets/img/logo.png" />
+                </div>
+
+                <div class="pesquisa">
+                    <input type="text" placeholder="Pesquisar" />
+                </div>
+
+                <div class="icones">
+                    {icons.map(icon => <ion-icon name={`${icon}-outline`}></ion-icon>)}
+                </div>
+
+                <MobileIcone class="icones-mobile" name="paper-plane-outline" />
+
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
