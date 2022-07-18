cdabffc2195e356f2edfb948a561ca82efe9bd76
lucasajajpiccoli <lucasajaj@gmail.com>
2022-07-18 17:12:56 -0300

    Requisitos satisfeitos - Versão para entrega

--- src/Componentes/Post.js
+++ src/Componentes/Post.js
@@ -14,61 +14,44 @@ function Topo(props) {
     );
 }
 
-function Conteudo(props) {
-    return (
-        <div class="conteudo">
-            <img src={`assets/img/${props.img}.svg`} />
-        </div>
-    );
-}
 
-function Acoes() {
+function Post(props) {
     const [likeStatus, setLikeStatus] = React.useState("-outline");
 
     function trocarLikeStatus() {
         setLikeStatus(likeStatus === "-outline" ? "" : "-outline");
     }
 
-    return (
-        <div class="acoes">
-            <div>
-                <ion-icon name={`heart${likeStatus}`} onClick={trocarLikeStatus}></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-        </div>
-    );
-}
+    function darLike() {
+        setLikeStatus("");
+    }
 
-function Curtidas(props) {
-    return (
-        <div class="curtidas">
-            <img src={`assets/img/${props.nome}.svg`} />
-            <div class="texto">
-                Curtido por <strong>{props.nome}</strong> e <strong>outras {props.numero} pessoas</strong>
-            </div>
-        </div>
-    );
-}
-
-function Fundo(props) {
-    return (
-        <div class="fundo">
-            <Acoes />
-            <Curtidas nome={props.nome} numero={props.numero} />
-        </div>
-    );
-}
-
-function Post(props) {
     return (
         <div class="post">
             <Topo nome={props.topoNome} />
-            <Conteudo img={props.img} />
-            <Fundo nome={props.curtidasNome} numero={props.numero} />
+
+            <div class="conteudo" onClick={darLike}>
+                <img src={`assets/img/${props.img}.svg`} />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name={`heart${likeStatus}`} onClick={trocarLikeStatus}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon name="bookmark-outline"></ion-icon>
+                    </div>
+                </div>
+                <div class="curtidas">
+                    <img src={`assets/img/${props.curtidasNome}.svg`} />
+                    <div class="texto">
+                        Curtido por <strong>{props.curtidasNome}</strong> e <strong>outras {props.numero} pessoas</strong>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
